5231f12c12f3bcaa875a25ee943a9c5a141bdda9
Devish Mittal <devishmittal1@gmail.com>
2025-12-10 22:38:50 +0530

    glass like effect

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import QuizContainer from "@/components/quiz/QuizContainer";
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen items-center justify-center p-4 md:p-8"
+      className="flex min-h-screen items-center justify-center p-4 md:py-[80px] md:px-[148px]"
       style={{
         background: "linear-gradient(107.96deg, #BECFEE 0%, #71C6E2 50%, #D9F4FA 75%, #BECFEE 100%)"
       }}
     >
-      <div className="w-full max-w-5xl rounded-[40px] p-2" style={{
-        backdropFilter: "blur(200px)",
-        boxShadow: "0px 4px 24px -1px rgba(0, 0, 0, 0.2)"
-      }}>
+      <div
+        className="w-full h-full rounded-[50px] relative flex items-center justify-center overflow-hidden"
+        style={{
+          background: "rgba(255, 255, 255, 0.3)",
+          backdropFilter: "blur(100px)",
+          // border: "2px solid rgba(255, 255, 255, 0.5)",
+          boxShadow: "0px 4px 50px rgba(0, 0, 0, 0.1)"
+        }}>
         <QuizContainer />
       </div>
     </main>
