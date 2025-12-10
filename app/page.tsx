d990ccc3163fc154870033ea55979be7e6ef431b
Devish Mittal <devishmittal1@gmail.com>
2025-12-10 22:46:41 +0530

    add another conatiner for question

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,17 @@ export default function Home() {
       }}
     >
       <div
-        className="w-full h-full rounded-[50px] relative flex items-center justify-center overflow-hidden"
+        className="w-full h-full rounded-[50px] relative flex items-center justify-center overflow-hidden py-[32px] px-[42px]"
         style={{
           background: "rgba(255, 255, 255, 0.3)",
           backdropFilter: "blur(100px)",
           border: "1.5px solid rgba(255, 255, 255, 0.5)",
           boxShadow: "0px 4px 50px rgba(0, 0, 0, 0.3)"
         }}>
-        <QuizContainer />
+        <div className="w-full h-full rounded-[42px] relative overflow-hidden"
+          style={{ background: "#F4FDFF" }}>
+          <QuizContainer />
+        </div>
       </div>
     </main>
   );
